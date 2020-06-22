dde2e84e23f4273ab24b3f53eefbcd1216ecddde
Kathya <kathyawhite00@gmail.com>
2020-06-22 11:53:23 -0500

    Add project files.

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+
+
+            var areaOfCircle = CalculateArea(20);
+
+            Console.WriteLine($"The area of a circle with radius of 20 is {CalculateArea(20)}");
+        }
+
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);
+        }
+    }
+}
